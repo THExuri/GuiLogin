07c3ecdc5f8c15e6c6260fe45684d32037757b12
未雨屏 <wyp999200637@qq.com>
2025-10-21 15:57:59 +0800

    添加Config，未实现其功能

--- src/main/java/org/weiyuping/guilogin/GuiLogin.java
+++ src/main/java/org/weiyuping/guilogin/GuiLogin.java
@@ -1,7 +1,9 @@
 package org.weiyuping.guilogin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.weiyuping.guilogin.commands.GuiLoginCommands;
 import org.weiyuping.guilogin.listener.*;
 
 public final class GuiLogin extends JavaPlugin {
@@ -14,7 +16,8 @@ public final class GuiLogin extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-        this.getLogger().info(ChatColor.AQUA +"GuiLogin 启动成功");
+        this.getLogger().info(ChatColor.AQUA + "GuiLogin 启动成功");
+        saveDefaultConfig();
         getServer().getPluginManager().registerEvents(new JoinListener(), this);
         getServer().getPluginManager().registerEvents(new ItemListener(), this);
         getServer().getPluginManager().registerEvents(new NumberPaneClickListener(), this);
@@ -22,10 +25,15 @@ public final class GuiLogin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new OnRegisterListener(), this);
         getServer().getPluginManager().registerEvents(new LoginListener(), this);
         getServer().getPluginManager().registerEvents(new CustomItemClickListener(), this);
+
+        if (Bukkit.getPluginCommand("guilogin") != null) {
+            Bukkit.getPluginCommand("guilogin").setExecutor(new GuiLoginCommands());
+            Bukkit.getPluginCommand("guilogin").setTabCompleter(new GuiLoginCommands());
+        }
     }
 
     @Override
     public void onDisable() {
-        this.getLogger().info(ChatColor.AQUA +"GuiLogin 已停止");
+        this.getLogger().info(ChatColor.AQUA + "GuiLogin 已停止");
     }
 }
