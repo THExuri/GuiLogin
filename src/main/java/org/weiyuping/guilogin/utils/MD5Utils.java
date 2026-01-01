63b3a22a02dbe87755ae6aa45168a422ccb2b281
未雨屏 <wyp999200637@qq.com>
2026-01-01 18:56:28 +0800

    简化错误及代码

--- src/main/java/org/weiyuping/guilogin/utils/MD5Utils.java
+++ src/main/java/org/weiyuping/guilogin/utils/MD5Utils.java
@@ -39,11 +39,6 @@ public class MD5Utils {
             return null;
         }
     }
-
-    public static boolean valid(final String text, final String md5) {
-        return md5.equals(encrypt(text));
-    }
-
     static {
         hex = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
     }
